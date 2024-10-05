6721c46935197b16e16a8647c44008d2bbb071c4
Jim2510 <gmguglielmino@gmail.com>
2024-10-05 19:46:20 +0200

    add exercises

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ x.forEach((el) => {
   console.log(el);
 });
 
-// ESERCIO 1: Dichiarare una variabile chiamata x e assegnarle il valore 5. Stampare il valore della variabile sulla console
+// ESERCIZIO 1: Dichiarare una variabile chiamata x e assegnarle il valore 5. Stampare il valore della variabile sulla console
 // Nota: il nome delle variabili deve essere inizializzato con una lettera minuscola
 
 //Dichiarare una costante chiamata "PI" e assegnarle il valore di 3.14. Stampare il valore della costante sulla console.
+
+//Dichiarare una variabile chiamata "nome" e assegnarle il valore "Marco". Successivamente, modificare il valore della variabile in "Luca". Stampare il nuovo valore sulla console.
+
+//Dichiarare una variabile chiamata "saluto" e assegnarle il valore "Ciao, come stai?". Stampare il valore della variabile sulla console.
+
+//Dichiarare una variabile chiamata "vero" e assegnarle il valore true. Stampare il valore della variabile sulla console.
