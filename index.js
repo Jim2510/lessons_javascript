6c433f9bf9cc54978f197aebb388312281843240
Jim2510 <gmguglielmino@gmail.com>
2024-10-05 23:04:27 +0200

    new exercises added

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const x = [1, 2, 3];
-
-x.forEach((el) => {
-  console.log(el);
-});
-
 // ESERCIZIO 1: Dichiarare una variabile chiamata x e assegnarle il valore 5. Stampare il valore della variabile sulla console
 // Nota: il nome delle variabili deve essere inizializzato con una lettera minuscola
 
@@ -14,3 +8,30 @@ x.forEach((el) => {
 //Dichiarare una variabile chiamata "saluto" e assegnarle il valore "Ciao, come stai?". Stampare il valore della variabile sulla console.
 
 //Dichiarare una variabile chiamata "vero" e assegnarle il valore true. Stampare il valore della variabile sulla console.
+
+// ESERCIZIO 2: Crea le variabili necessarie per specificare nome, cognome, colore preferito e anno di nascita.
+// Dichiara una variabile che concateni tutte le variabili precedenti e stampala in console
+
+// ESERCIZIO 3: Crea 3 variabili x, y e area e utilizza il valore di x e y per calcolare l'area di un rettangolo.
+
+// In Javascript il metodo if else permette di verificare se una condizione è vera o falsa e inserire all'interno di uno dei blocchi
+// una porzione di codice eseguibile se la condizione viene rispettata
+
+// ES:
+
+let x = 12;
+
+if (typeof x === "number") {
+  console.log("x is a number");
+} else if (typeof x === "string") {
+  console.log("x is a string");
+} else {
+  console.log("x is not a number or a string");
+}
+
+// ESERCIZIO 4: Scrivi un programma che controlli se un numero è positivo, negativo o zero e stampi il risultato in console.
+
+// ESERCIZIO 5: Scrivi un programma che controlli se un numero è divisibile per 3 e per 5 e stampi il risultato in console.
+
+// ESERCIZIO 6: Scrivi un programma che controlli se un numero è pari o dispari e stampi il risultato in console.
+// HINT: per verificare se un numero è pari o dispari si può utilizzare la condizione % 2 === 0
