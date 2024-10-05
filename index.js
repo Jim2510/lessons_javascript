1a00036bc446b938e627ffb3fc412dfb879b7bb1
Jim2510 <gmguglielmino@gmail.com>
2024-10-05 23:25:41 +0200

    new exercises added

--- index.js
+++ index.js
@@ -19,15 +19,20 @@
 
 // ES:
 
-let x = 12;
+let myVariable = 12;
 
-if (typeof x === "number") {
+if (typeof myVariable === "number") {
   console.log("x is a number");
-} else if (typeof x === "string") {
+} else if (typeof myVariable === "string") {
   console.log("x is a string");
 } else {
   console.log("x is not a number or a string");
 }
+
+// NOTA: il metodo typeof restituisce il tipo di dato di una variabile
+
+const type = typeof myVariable;
+console.log(type); // Output: number
 
 // ESERCIZIO 4: Scrivi un programma che controlli se un numero è positivo, negativo o zero e stampi il risultato in console.
 
@@ -35,3 +40,9 @@ if (typeof x === "number") {
 
 // ESERCIZIO 6: Scrivi un programma che controlli se un numero è pari o dispari e stampi il risultato in console.
 // HINT: per verificare se un numero è pari o dispari si può utilizzare la condizione % 2 === 0
+
+// ARRAY: l'array è una struttura dati che permette di raggruppare un insieme di valori all'interno di una variabile alla quale si può accedere tramite un indice numerico
+// Esempio:
+const myArray = [1, 2, 3, 4, 5];
+// Gli array in javascript permettono di salvare all'interno qualsiasi tipo di dato
+const myArray2 = [1, "stringa", true, null, undefined, { name: "Mario" }];
